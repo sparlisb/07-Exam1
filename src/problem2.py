94c577060ba8d708fce86d182b75cb847b9864fb
sparlisb <sparlisb@rose-hulman.edu>
2018-09-17 23:06:05 -0400

    exam

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Valerie Galluzzi, Mark Hays,
-         Amanda Stouder, their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Amanda Stouder, their colleagues and Shamus Sparling.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -20,7 +20,7 @@ def main():
 def problem2a_testing():
     """ Tests the   sum_of_digits_of_sum_of_factors   function. """
     ############################################################################
-    #  TODO: 2. Implement this TEST function.  See the IMPORTANT NOTE below.
+    #  done: 2. Implement this TEST function.  See the IMPORTANT NOTE below.
     #     This problem TESTS the    sum_of_digits_of_sum_of_factors    function
     #     that is defined below.  Include at least **   4   ** tests.
     #     Use the usual format:
@@ -31,9 +31,9 @@ def problem2a_testing():
     #       print('Expected:', expected)
     #       print('Actual:  ', actual)
     #
-    #  TODO (continued): IMPORTANT NOTE:
-    #  TODO (continued):   The function that you are TESTING is PURPOSELY
-    #  TODO (continued)    NOT implemented.  Do NOT implement it.  That is,
+    #  done (continued): IMPORTANT NOTE:
+    #  done (continued):   The function that you are TESTING is PURPOSELY
+    #  done (continued)    NOT implemented.  Do NOT implement it.  That is,
     #                   do NOT implement the
     #                           sum_of_digits_of_sum_of_factors
     #                   function!  Just write these TESTS of that function
@@ -44,6 +44,21 @@ def problem2a_testing():
     print('---------------------------------------------------------')
     print('Testing the   sum_of_digits_of_sum_of_factors   function:')
     print('---------------------------------------------------------')
+    print('Test 1:')
+    print('Expected: 11')
+    print('Actual:', sum_of_digits_of_sum_of_factors(28))
+    print()
+    print('Test 2:')
+    print('Expected: 9')
+    print('Actual:', sum_of_digits_of_sum_of_factors(10))
+    print()
+    print('Test 3:')
+    print('Expected: 7')
+    print('Actual:', sum_of_digits_of_sum_of_factors(4))
+    print()
+    print('Test 4:')
+    print('Expected: 4')
+    print('Actual:', sum_of_digits_of_sum_of_factors(25))
 
 
 def sum_of_digits_of_sum_of_factors(n):
@@ -63,19 +78,19 @@ def sum_of_digits_of_sum_of_factors(n):
        *** ASK FOR AN EXPLANATION IF YOU DO NOT UNDERSTAND THE ABOVE. ***
     """
     ############################################################################
-    #  TODO (continued):  This function is PURPOSELY ** not implemented. **
-    #  TODO (continued):  DO NOT IMPLEMENT  sum_of_digits_of_sum_of_factors.
+    #  done (continued):  This function is PURPOSELY ** not implemented. **
+    #  done (continued):  DO NOT IMPLEMENT  sum_of_digits_of_sum_of_factors.
     #                     Just leave it as it is (with no code).
     ############################################################################
 
 
 ################################################################################
-# TODO: 3.  READ the green doc-string for the   number_of_factors   function
+# done: 3.  READ the green doc-string for the   number_of_factors   function
 #    defined below.  You do NOT need to understand its implementation,
 #    just its specification (per the doc-string).
 #    You should  ** CALL **  that function as needed in implementing
 #    the other functions.
-# TODO (continued):  After you have READ this _TODO_, change its _TODO_ to DONE.
+# done (continued):  After you have READ this _TODO_, change its _TODO_ to DONE.
 ################################################################################
 
 def number_of_factors(n):
@@ -102,8 +117,8 @@ def number_of_factors(n):
 
     return count
     ############################################################################
-    #  TODO (continued):  Students: Do NOT touch the above   number_of_factors
-    #  TODO (continued)   function; CALL it as appropriate in problem(s) below.
+    #  done (continued):  Students: Do NOT touch the above   number_of_factors
+    #  done (continued)   function; CALL it as appropriate in problem(s) below.
     ############################################################################
 
 
@@ -310,15 +325,19 @@ def problem2b(a, b, x):
          and the sum   16 + 18 + 20 + 24   is 78.
      """
     ############################################################################
-    # TODO: 4. Implement and test this function.  See the IMPORTANT NOTE below!
+    # done: 4. Implement and test this function.  See the IMPORTANT NOTE below!
     #          Tests have been written for you (above).
     ############################################################################
-    # TODO (continued): IMPORTANT NOTE:
-    # TODO (continued):   **  For full credit you must appropriately use
-    # TODO (continued):       (i.e., call) the   number_of_factors   function
+    # done (continued): IMPORTANT NOTE:
+    # done (continued):   **  For full credit you must appropriately use
+    # done (continued):       (i.e., call) the   number_of_factors   function
     #                         that is DEFINED ABOVE.
     ############################################################################
-
+    total = 0
+    for k in range(b - a + 1):
+        if number_of_factors(a + k) >= x:
+            total = total + a + k
+    return total
 
 def run_test_problem2c():
     """ Tests the   problem2c   function. """
@@ -442,7 +461,11 @@ def problem1c(m):
     # TODO (continued)    You get   NO credit   if your solution is more
     # TODO (continued)    than 3 lines of code (and it can be done in 1 line).
     ############################################################################
-
+    total = 0
+    for k in range(9):
+        if number_of_factors(m + k + 1) == 2:
+            total = total + m + k + 1
+    return total
 
 ################################################################################
 # Our tests use the following to print error messages in red.
--- src/problem3.py
+++ src/problem3.py
@@ -2,7 +2,7 @@
 Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Valerie Galluzzi, Mark Hays,
-         Amanda Stouder, their colleagues and PUT_YOUR_NAME_HERE.
+         Amanda Stouder, their colleagues and Shamus Sparling.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
@@ -74,7 +74,11 @@ def problem3(point, length, delta, window):
               The 2nd, 5th, 8th, etc rg.Lines have color 'cyan'
               The 3rd, 6th, 9th, etc rg.Lines have color 'spring green'
 
-      NOTE: The NUMBER of lines to draw is determined by the facts that:
+
+      :type length:  int
+      :type delta:   int
+      :type window:  rg.RoseWindow
+    """      NOTE: The NUMBER of lines to draw is determined by the facts that:
         -- The vertical line has the given length.
         -- All horizontal lines have their left endpoint on the vertical line.
         -- The distance between horizontal lines is the given delta.
@@ -83,28 +87,58 @@ def problem3(point, length, delta, window):
 
     Type hints:
       :type point:   rg.Point
-      :type length:  int
-      :type delta:   int
-      :type window:  rg.RoseWindow
-    """
     # --------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
-    # TODO (continued):  IMPORTANT: Use this ITERATIVE ENHANCEMENT PLAN:
-    # TODO (continued):    1. Make the sole VERTICAL line appear,
-    # TODO (continued):         with thickness 3.
-    # TODO (continued):    2. Make the FIRST horizontal line appear.
-    # TODO (continued):    3. Make MORE horizontal lines appear,
-    # TODO (continued):         each delta below the previous one.
-    # TODO (continued):    4. Make each successive horizontal line
-    # TODO (continued):         20 pixels longer than the previous one.
-    # TODO (continued):    5. Make the right NUMBER of horizontal lines.
-    # TODO (continued):    6. Make the horizontal lines each have thickness 3
-    # TODO (continued):         and colors per the specified pattern.
+    # done: 2. Implement and test this function.
+    # done (continued):  IMPORTANT: Use this ITERATIVE ENHANCEMENT PLAN:
+    # done (continued):    1. Make the sole VERTICAL line appear,
+    # done (continued):         with thickness 3.
+    # done (continued):    2. Make the FIRST horizontal line appear.
+    # done (continued):    3. Make MORE horizontal lines appear,
+    # done (continued):         each delta below the previous one.
+    # done (continued):    4. Make each successive horizontal line
+    # done (continued):         20 pixels longer than the previous one.
+    # done (continued):    5. Make the right NUMBER of horizontal lines.
+    # done (continued):    6. Make the horizontal lines each have thickness 3
+    # done (continued):         and colors per the specified pattern.
     #          Tests have been written for you (above).
     # --------------------------------------------------------------------------
-
+    point.attach_to(window)
+    line = rg.Line(point, rg.Point(point.x, point.y + length))
+    line.thickness = 3
+    line.attach_to(window)
+    start1 = point
+    start2 = rg.Point(point.x, start1.y + delta)
+    start3 = rg.Point(point.x, start2.y + delta)
+    end1 = rg.Point(start1.x + length, start1.y)
+    end2 = rg.Point(end1.x + 20, start2.y)
+    end3 = rg.Point(end2.x + 20, start3.y)
+    count = length//delta + 1
+    for k in range(length//delta):
+        line1 = rg.Line(start1, end1)
+        line1.color = 'magenta'
+        line2 = rg.Line(start2, end2)
+        line2.color = 'cyan'
+        line3 = rg.Line(start3, end3)
+        line3.color = 'spring green'
+        if count != 0:
+            count = count - 1
+            line1.attach_to(window)
+        if count != 0:
+            count = count - 1
+            line2.attach_to(window)
+        if count != 0:
+            count = count - 1
+            line3.attach_to(window)
+        start1 = rg.Point(point.x, start3.y + delta)
+        end1 = rg.Point(end3.x + 20, end3.y + delta)
+        start2 = rg.Point(point.x, start1.y + delta)
+        end2 = rg.Point(end1.x + 20, end1.y + delta)
+        start3 = rg.Point(point.x, start2.y + delta)
+        end3 = rg.Point(end2.x + 20, end2.y + delta)
+    window.render()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
 main()
+
